64db51e367237d9939aa437f958d664901f99bfa
David Martin-Leibbrand <36173978+DavidM7766@users.noreply.github.com>
2018-02-05 16:42:45 -0600

    add readFile method to update data.json

--- fs_demo.js
+++ fs_demo.js
@@ -1,3 +1,5 @@
+import { error } from 'util';
+
 var fs = require('fs')
 
 // read from a file
@@ -6,4 +8,18 @@ fs.readFile('./data.json', (err, data) =>
     if(err)
         return console.error(err)
     console.log(JSON.parse(data));
+})
+
+var mydata = 
+{
+    "name" : "jane",
+    "message" : "Hi there!"
+}
+
+// write to a file
+fs.writeFile('./data.json', JSON.stringify(myData), (error) =>
+{
+    if(error)
+        return console.error(err)
+    console.log('File written successfully')
 })
